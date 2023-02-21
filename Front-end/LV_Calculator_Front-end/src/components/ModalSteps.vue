5c2ed915c135e351b7a52894cbe12caf90cd58d3
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-02-22 00:13:31 +0200

    modals under the i button ->content ->tablet ->mobile

--- Front-end/LV_Calculator_Front-end/src/components/ModalSteps.vue
+++ Front-end/LV_Calculator_Front-end/src/components/ModalSteps.vue
@@ -1,23 +1,49 @@
 <script setup>
    import ButtonCloseModal from "./ButtonCloseModal.vue";
+   import ButtonScroll from "./ButtonScroll.vue";
 </script>
 
 <template>
-    <div class="modal-steps-mask">
-        <div class="modal-steps-wrapper">
-            <div class="modal-steps-block">
+    <div class="modal-mask steps">
+        <div class="modal-wrapper-steps">
+            <div class="modal-block-steps">
                 <ButtonCloseModal @closeModal="$emit('closeModal')"></ButtonCloseModal>
-                <div class="modal-content">
-                    <div class="modal-content__img-container">
-                       <img src="../assets/images/example_steps.png" alt="img">
-                    </div>
-                    <div class="modal-content__text-container">
-                        <h2>This is heading</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam quasi voluptas dolore soluta, nulla quis eos illum aliquid, dignissimos laudantium consequuntur quaerat provident cupiditate ipsum beatae itaque quibusdam deserunt!</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam quasi voluptas dolore soluta, nulla quis eos illum aliquid, dignissimos laudantium consequuntur quaerat provident cupiditate ipsum beatae itaque quibusdam deserunt!</p>
+                <h3>Ambīciju līmeņi</h3>
+                <div class="content-container steps">
+                    <div class="content-blocks">
+                        <div class="content-block">
+                            <div class="content-block__text-container">
+                                <h2>Ambīciju līmeņi</h2>
+                                <p>Ambīciju līmeņi katrā no sektoriem ir atšķirīgi un pielāgoti attiecīgajai nozarei, bet visi seko līdzīgiem pieņēmumiem:</p>
+                                <img class="img-mobile" src="../assets/images/example_steps_mobile.png" alt="example steps mobile">
+                                <ol>
+                                    <li>līmenis - lielas pārmaiņas attiecīgajā sektorā netiek plānotas līdz 2050.gadam;</li>
+                                    <li>līmenis - dažas “minimālas piepūles” izmaiņas tiek veiktas līdz 2050.gadam (atkarīgas no katra sektora izvirzītajiem mērķiem un iespējām);</li>
+                                    <li>līmenis - tiek veiktas izmaiņas, kas ir stipri ambiciozākas, bet uzskatāmas par iespējamām;</li>
+                                    <li>līmenis - iespaidīgas izmaiņas, kas nav viegli sasniedzamas vai pat vēlamas. Šīs ir ekspertu ieskatā ļoti grūti realizējamas izmaiņas no tehnoloģiskā viedoļa.</li>
+                                </ol>
+                            </div>
+                            <div class="content-block__img-container">
+                               <img class="img-tablet" src="../assets/images/example_steps_tablet.png" alt="example steps tablet">
+                            </div>
+                        </div>
+                        <div class="content-block">
+                            <div class="content-block__text-container">
+                                <p>Pie katra no amīciju līmeņiem ir iespējams iepazīties ar īsu tā aprakstu (novietojot kursoru uz izvēlētā ambīciju līmeņa, vai mobilajā versijā uzklikšķinot uz izvēlētā ambīciju līmeņa), kā arī atsevišķiem apakšsektotiem ir pieejami garāki apraksti (pdf dokumenti) ar izvērstāku skaidrojumu par attiecīgo nozari.</p>
+                                <p>Balstoties uz jūsu izvēlēto ambīciju līmeni, ko izvēlaties katrai nozarei, kalkulatora modelis aprēķina enerģijas un materiālu patēriņu, siltumnīcefekta gāzu emisijas un no tā izrietošo ietekmi uz sabiedrību, vidi un zemes izmantošanu, kas tiek reprezentēta oglekļa emisijas ekvivalentos (CO2ekv).</p>
+                            </div>
+                            <div class="content-block__text-container">
+                                <ol>
+                                    <li>Ambīciju līmeņa neliels apraksts.</li>
+                                    <li>Sektoru ambīciju izvēlne.</li>
+                                    <li>Apakšsektoru ambīciju izvēlne.</li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
+                <ButtonScroll></ButtonScroll>
             </div>
         </div>
     </div>
@@ -28,6 +54,7 @@ export default {
     name: 'ModalSteps',
     components: {
             ButtonCloseModal,
+            ButtonScroll
         }
 }
 </script>
